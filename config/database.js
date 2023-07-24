5b44eac0ad59c57ca3da94391859ca85130222e9
QeApps-Rs <rishabh@qepps.com>
2023-07-24 11:41:20 +0530

    first commit

---
+++ config/database.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'db156.pair.com',
+  user     : '1024899_52',
+  password : 'n5yVHaS4t9Nzwus4',
+  database : 'qetailcom_mindzai'
+});
+connection.connect(function(err) {
+if (err)
+{
+    console.error('Error connecting Db : ' + err.stack);
+    return;
+}
+});
+module.exports = connection;
---
+++ readme.txt
@@ -0,0 +1,3 @@
+App URL - http://localhost:3000/
+Allowed redirection URL(s) - http://localhost:3000/auth/shopify/callback
+Env HOST_SCHEME = http , https
